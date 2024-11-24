760c12361cb680b37f9fe27d5c0170cfecc987ea
wayanad house <150238196+wayanadiitm@users.noreply.github.com>
2024-11-24 22:57:17 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'SeHAS | Secure Hardware, Architectures and Software ';
-export const SITE_DESCRIPTION = 'The international workshop on Secure Hardware, Architectures, and Software (SeHAS 2024), we invite leading researchers in the field to delve into the latest security threats, solutions, and emerging technologies.';
+export const SITE_TITLE = 'LogicLooM 3.0 | Margazhi 2025';
+export const SITE_DESCRIPTION = 'Welcome to LogicLooM, the celebrated technical event at Paradox.';
 export const GENERATE_SLUG_FROM_TITLE = false
 export const TRANSITION_API = true
