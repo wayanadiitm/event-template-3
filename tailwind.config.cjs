085cebf6f2b7c6d2a13ef58c10359534d36157e9
goutham-m7 <mgoutham1975@gmail.com>
2024-11-25 17:32:31 +0530

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -24,23 +24,3 @@ module.exports = {
 	},
 	darkMode: 'selector'
 }
-
-
-module.exports = {
-	//...
-	daisyui: {
-	  themes: [
-		{
-		  mytheme: {
-			"primary": "#a991f7",
-			"secondary": "#f6d860",
-			"accent": "#37cdbe",
-			"neutral": "#3d4451",
-			"base-100": "#ffffff",
-		  },
-		},
-		"dark",
-		"cupcake",
-	  ],
-	},
-  }
