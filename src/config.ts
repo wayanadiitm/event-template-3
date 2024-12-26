d89da74f14f9360208911ea285ef234c027d5bf0
wayanad house <150238196+wayanadiitm@users.noreply.github.com>
2024-12-27 00:33:10 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'LogicLooM 3.0 | Margazhi 2025';
-export const SITE_DESCRIPTION = 'Welcome to LogicLooM, the celebrated technical event at Paradox.';
+export const SITE_DESCRIPTION = 'Welcome to LogicLooM, the most celebrated technical event at Paradox, the annual flagship techno-cultural fest for IIT Madras BS students';
 export const GENERATE_SLUG_FROM_TITLE = false
 export const TRANSITION_API = true
