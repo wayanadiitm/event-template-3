12aadd1847c8ba952d492d2b33ef97316b4d7bad
wayanad house <150238196+wayanadiitm@users.noreply.github.com>
2025-01-10 14:52:56 +0530

    Delete src directory

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'LogicLooM 3.0 | Margazhi 2025';
-export const SITE_DESCRIPTION = 'Welcome to LogicLooM, the most celebrated technical event at Paradox, the annual flagship techno-cultural fest for IIT Madras BS students';
-export const GENERATE_SLUG_FROM_TITLE = false
-export const TRANSITION_API = true
